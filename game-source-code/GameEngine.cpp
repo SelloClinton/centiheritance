ed2a181f4fa1946b112f9e021c3a0b92bb03fecc
SelloClinton <680492@students.wits.ac.za>
2018-10-01 00:22:40 +0200

    track GameEngine

---
+++ game-source-code/GameEngine.cpp
@@ -0,0 +1,102 @@
+#include "GameEngine.h"
+
+GameEngine::GameEngine():
+			display_(make_shared<Display>())
+			,drawer_(make_shared<Drawer>(display_->getWindow()))
+			,position_(make_shared<Position>(Constants::DISPLAY_WIDTH_/2,(Constants::DISPLAY_HEIGHT_-50)))
+            ,mover(make_shared<Mover>(position_,5))
+			,player_(make_shared<Player>(mover))
+            ,centipede_(make_shared<Centipede>(Constants::INITIAL_SIZE_OF_CENTIPEDE_))
+			,field_(make_shared<Field>(Constants::NUMBER_OF_MUSHROOMS))
+			,playing_(true)
+			,game_over_(false)
+{
+		if (display_== nullptr) throw WindowNotCreated();
+}
+
+void GameEngine::playGame(){
+	splashScreen();
+	
+	while(display_->isOpen()){
+			
+		if((playing_)&&(!game_over_)){//playing
+			checkInput();
+			update();
+			drawObjects();
+			display_->display();
+			display_->clear();
+					
+		}
+		else if ((!playing_)&&(!game_over_)){//paused
+			checkInput();
+			drawObjects();
+			displayPauseMessage();
+		}
+		else if(game_over_){//game over
+			checkInput();
+			drawObjects();
+			displayGameOverMessage();
+
+		}
+	}
+}
+
+void GameEngine::splashScreen(){
+	SplashScreen welcome_screen(display_->getWindow());
+	welcome_screen.drawSplashScreen();
+}
+
+void GameEngine::update(){
+		auto updator = make_shared<Update>();
+		updator->updateGame(player_,key_,centipede_,field_);	
+		gameStatus();
+
+}
+
+void GameEngine::checkInput(){
+	
+	KeyReader key_read;
+	key_ = key_read.readKey(display_->getWindow());
+	
+	if(key_ == Pressed::P)
+		playing_ = !playing_;
+	else if(key_ == Pressed::ESCAPE)
+		display_->close();
+}
+
+void GameEngine::drawObjects(){
+	drawer_->drawField(field_);
+    drawer_->drawPlayer(player_);
+    drawer_->drawCentipede(centipede_);
+	drawer_->drawBullets(player_);
+
+}
+void GameEngine::displayPauseMessage(){
+	drawer_->drawPauseMessage();
+	display_->display();
+	display_->clear();
+}
+void GameEngine::displayGameOverMessage(){
+	if(auto over = "won";player_->attribute()->isLive()){
+		drawer_->drawGameOverMessage(over);
+		display_->display();
+		display_->clear();
+	}
+	else if(auto over = "lost";!player_->attribute()->isLive()){
+		drawer_->drawGameOverMessage(over);
+		display_->display();
+		display_->clear();
+	}
+}
+
+
+
+
+void GameEngine::gameStatus(){
+		if ((!player_->attribute()->isLive())||(centipede_->getCentipede().empty())){
+						game_over_ = true;
+						playing_ = false;
+			
+		}
+}
+
---
+++ game-source-code/GameEngine.h
@@ -0,0 +1,62 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include "Display.h"
+#include "Drawer.h"
+#include "KeyReader.h"
+#include "Mover.h"
+#include "Player.h"
+#include "Position.h"
+#include "SplashScreen.h"
+#include "Centipede.h"
+#include "Constants.h"
+#include "Box.h"
+#include "CollisionDetection.h"
+#include "CollisionReaction.h"
+#include "Field.h"
+#include "Mushroom.h"
+#include "Update.h"
+
+#include <memory>
+#include <list>
+using namespace std;
+using std::shared_ptr;
+using std::make_shared;
+using std::unique_ptr;
+using std::make_unique;
+
+class GameEngine{
+		
+public:
+	GameEngine();
+	void playGame();
+private:
+	shared_ptr<Display> display_;
+	shared_ptr<Drawer> drawer_;
+	shared_ptr<Position> position_;
+    shared_ptr<Position> pos;
+    shared_ptr<Mover> mover;
+    shared_ptr<Mover> mover2;
+	shared_ptr<Player> player_;
+	shared_ptr<Centipede> centipede_;
+	shared_ptr<Field> field_;
+	Pressed key_;
+	bool game_over_;
+	bool playing_;
+	
+	void splashScreen();
+	void update();
+    void updateCentipede(shared_ptr<Field> field);
+	void checkInput();
+//	void keyReaction(Pressed key);
+	void handleInput();
+	void drawObjects();
+	void displayPauseMessage();
+	void displayGameOverMessage();
+	bool checkCollision();
+	void checkPlayerCollision();
+	void gameStatus();
+	void checkBulletMushroomCollision();
+};
+
+#endif
